fc231f6c89509aa221539d362a6b846b29148e6e
dewert99 <dewert10@gmail.com>
2024-03-12 10:04:06 -0700

    Switched to using random sample of benchmarks

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,24 +1,19 @@
 use bat_egg_smt::interp_smt2;
 use codspeed_criterion_compat::{black_box, criterion_group, criterion_main, Criterion};
 use std::fs::File;
-use std::io::Read;
+use std::io::{read_to_string, Read};
 use std::path::Path;
 pub fn criterion_benchmark(c: &mut Criterion) {
-    use walkdir::WalkDir;
-
-    let path = Path::new("benches/starexec");
-    for x in WalkDir::new(path).into_iter().filter_map(Result::ok) {
-        let path = x.path();
-        if path.extension() == Some("smt2".as_ref()) {
-            let mut data = Vec::new();
-            let mut out = Vec::new();
-            let mut err = Vec::new();
-            File::open(path).unwrap().read_to_end(&mut data).unwrap();
-            c.bench_function(path.as_os_str().to_str().unwrap(), |b| {
-                b.iter(|| interp_smt2(&data, &mut out, &mut err))
-            });
-            black_box((out, err));
-        }
+    // Random sample of 5% of incremental QF_UF benchmark
+    let path = Path::new("benches/random_sample.txt");
+    let sample = read_to_string(File::open(path).unwrap()).unwrap();
+    for path in sample.lines() {
+        let mut data = Vec::new();
+        let mut out = Vec::new();
+        let mut err = Vec::new();
+        File::open(path).unwrap().read_to_end(&mut data).unwrap();
+        c.bench_function(path, |b| b.iter(|| interp_smt2(&data, &mut out, &mut err)));
+        black_box((out, err));
     }
 }
 
